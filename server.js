b7e878d6f4cdfef8a5769ea3f7fbd5526895d053
Panos Anastasiadis <anastapd@gmail.com>
2020-01-26 19:45:13 +0000

    Implemented http logging when in development

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const dotenv = require('dotenv');
 const bootcamps = require('./routes/bootcamps');
 const logger = require('./middleware/logger');
+const morgan = require('morgan');
 
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.use(logger);
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
