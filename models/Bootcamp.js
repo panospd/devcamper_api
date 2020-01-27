89a47fd8b4b55815b7a23c9f75621abe47b8b617
Panos Anastasiadis <anastapd@gmail.com>
2020-01-27 21:16:11 +0000

    connected to mongo db atlas and created basic Bootcamp model using mongoose

---
+++ models/Bootcamp.js
@@ -0,0 +1,101 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    unique: true,
+    trim: true,
+    maxlength: [50, 'Name cannot be more than 50 characters']
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, 'Please add a description'],
+    maxlength: [500, 'DEscription cannot be more than 500 characters']
+  },
+  website: {
+    type: String,
+    match: [
+      /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/,
+      'Please use a valid URL with http or https'
+    ]
+  },
+  phone: {
+    type: String,
+    maxlength: [20, 'Phone number cannot be longer than 20 characters']
+  },
+  email: {
+    type: String,
+    match: [
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please add a valid email'
+    ]
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add an address']
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: '2dsphere'
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String
+  },
+  careers: {
+    type: [String],
+    required: true,
+    enum: [
+      'Web Development',
+      'Mobile Development',
+      'UI/UX',
+      'Data Science',
+      'Business',
+      'Other'
+    ]
+  },
+  averageRating: {
+    type: Number,
+    minlength: [1, 'Rating must be at least 1'],
+    minlength: [10, 'Rating cannot be more than 10']
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: 'no-photo.jpg'
+  },
+  housing: {
+    type: Boolean,
+    default: false
+  },
+  jobAssistance: {
+    type: Boolean,
+    default: false
+  },
+  jobGuarantee: {
+    type: Boolean,
+    default: false
+  },
+  acceptGi: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Bootcamp', schema);
--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const dotenv = require('dotenv');
 const bootcamps = require('./routes/bootcamps');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDb = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
+
+connectDb();
 
 const app = express();
 
@@ -13,8 +17,16 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`
+    `Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`.yellow
+      .bold
   );
+});
+
+process.on('unhandledRejection', (error, promise) => {
+  console.log(`Error: ${error.message}`.red);
+  server.close(() => {
+    process.exit(1);
+  });
 });
