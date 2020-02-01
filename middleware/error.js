adc5e279fe6f34923fdec250f00d9ac460ef2712
Panos Anastasiadis <anastapd@gmail.com>
2020-02-01 18:45:35 +0000

    Implemented validation for bootcamps

--- middleware/error.js
+++ middleware/error.js
@@ -5,14 +5,24 @@ const errorHandler = (err, req, res, next) => {
 
   error.message = err.message;
 
-  console.log(err.stack.red);
+  console.log(err);
 
   if (err.name === 'CastError') {
     const message = `Bootcamp not found with id of ${error.value}`;
     error = new ErrorResponse(message, 404);
   }
 
-  res.status(error.statusCode).json({
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(e => e.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
     success: false,
     error: error.message || 'Server error'
   });
