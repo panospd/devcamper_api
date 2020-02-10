c3fb259b1b70c9608cc4a60dde4f3bf41d4a6fac
Panos Anastasiadis <anastapd@gmail.com>
2020-02-10 16:40:10 +0000

    Implemented pagination for Bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -11,7 +11,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
   const reqQuery = { ...req.query };
 
-  const removeFields = ['select', 'sort'];
+  const removeFields = ['select', 'sort', 'page', 'limit'];
 
   removeFields.forEach(field => delete reqQuery[field]);
 
@@ -36,13 +36,37 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     query = query.sort('-createdAt');
   }
 
-  console.log(query);
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query.skip(startIndex).limit(limit);
+
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    };
+  }
 
   const bootcamps = await query;
 
   res.status(200).json({
     success: true,
     count: bootcamps.length,
+    pagination,
     data: bootcamps
   });
 });
