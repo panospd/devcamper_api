04b2c729f64ce96c672856f6288db0b0c36def21
Panos Anastasiadis <anastapd@gmail.com>
2020-02-13 21:57:32 +0000

    Implemented login for user and returna json token

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,12 +3,10 @@ const User = require('../models/User');
 const asyncHandler = require('../middleware/async');
 
 // @desc    Register user
-// @route   Get /api/v1/auth/register
+// @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
-
-  console.log(password);
 
   const user = await User.create({
     name,
@@ -16,6 +14,33 @@ exports.register = asyncHandler(async (req, res, next) => {
     password,
     role
   });
+
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({
+    success: true,
+    token
+  });
+});
+
+// @desc    Login user
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return next(
+      new ErrorResponse('Please provide an email and a password', 400)
+    );
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) return next(new ErrorResponse('Invalid credentials', 401));
+
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) return next(new ErrorResponse('Invalid credentials', 401));
 
   const token = user.getSignedJwtToken();
 
