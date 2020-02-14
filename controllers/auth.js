676bc2519b00aaa4a95b692a48e9b11ed658ff5c
panospd <anastapd@gmail.com>
2020-02-14 13:50:55 +0000

    Implemented creating httpOnly cookie with the token as value and return to the client

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
-const ErrorResponse = require('../utils/errorResponse');
-const User = require('../models/User');
-const asyncHandler = require('../middleware/async');
+const ErrorResponse = require("../utils/errorResponse");
+const User = require("../models/User");
+const asyncHandler = require("../middleware/async");
 
 // @desc    Register user
 // @route   POST /api/v1/auth/register
@@ -31,21 +31,37 @@ exports.login = asyncHandler(async (req, res, next) => {
 
   if (!email || !password)
     return next(
-      new ErrorResponse('Please provide an email and a password', 400)
+      new ErrorResponse("Please provide an email and a password", 400)
     );
 
-  const user = await User.findOne({ email }).select('+password');
+  const user = await User.findOne({ email }).select("+password");
 
-  if (!user) return next(new ErrorResponse('Invalid credentials', 401));
+  if (!user) return next(new ErrorResponse("Invalid credentials", 401));
 
   const isMatch = await user.matchPassword(password);
 
-  if (!isMatch) return next(new ErrorResponse('Invalid credentials', 401));
+  if (!isMatch) return next(new ErrorResponse("Invalid credentials", 401));
 
+  sendTokenResponse(user, 200, res);
+});
+
+const sendTokenResponse = (user, statusCode, res) => {
   const token = user.getSignedJwtToken();
 
-  res.status(200).json({
-    success: true,
-    token
-  });
-});
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === "production") options.secure = true;
+
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({
+      success: true,
+      token
+    });
+};
